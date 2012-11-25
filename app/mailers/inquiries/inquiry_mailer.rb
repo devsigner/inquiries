f73979c8c150d93f7d56ec05cb62bbedd61761bf
John Colvin <colvin.john@gmail.com>
2012-11-24 20:22:03 -0500

    Fixed a few errors in the mailer

--- app/mailers/inquiries/inquiry_mailer.rb
+++ app/mailers/inquiries/inquiry_mailer.rb
@@ -5,15 +5,15 @@ module Inquiries
       @inquiry = inquiry
       mail :subject   => Inquiries::Inquiry.confirmation_subject,
            :to        => inquiry.email,
-           :from      => "\"#{Rails.application.class.parent_name}\" <no-reply@#{request.domain}>",
+           :from      => "no-reply@#{request.domain}",
            :reply_to  => Inquiries::Inquiry.notification_recipients.split(',').first
     end
 
     def notification(inquiry, request)
       @inquiry = inquiry
       mail :subject   => Inquiries::Inquiry.notification_subject,
-           :to        => Refinery::Inquiries::Setting.notification_recipients,
-           :from      => "\"#{Rails.application.class.parent_name}\" <no-reply@#{request.domain}>",
+           :to        => Inquiries::Inquiry.notification_recipients,
+           :from      => "no-reply@#{request.domain}",
            :reply_to  => inquiry.email
     end
 
